966f82fb9142ec830fe9a42f28b5c684f7edee84
subhanwhetstonez <subhan.suleman@whetstonez.com>
2024-05-15 18:10:06 +0500

    adding array's values

--- main.js
+++ main.js
@@ -109,6 +109,16 @@ for (t = 0; t < nestarray.length; t++) {
 
 console.log(singlearray);
 
+// sum of the answered array
+function sumArray(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+console.log(sumArray(singlearray));
+
 //Age level
 function aging(a) {
   if (a >= 18) {
